d9aed3e8c150c142c982baae3e95c34baae073ca
rasm033d <54931680+rasm033d@users.noreply.github.com>
2020-06-05 04:44:29 +0200

    Update onecopy.py

--- onecopy.py
+++ onecopy.py
@@ -2,7 +2,8 @@ import os
 from pathlib import Path
 import random
 from shutil import copy
-import time 
+import time
+log = open("log.txt", "a") 
 path = r"\\192.168.0.112\Anonymous\SlowStorage\Testthings\Windows.iso"
 output = r"C:\Users\Rasmus D. Hansen\Documents\test"
 count = 0
@@ -11,4 +12,6 @@ while count < 100:
     copy(path, output)
     endTime = time.time() - StartTime
     print(endTime)
+    log.write("\n")
+    log.write(str(endTime))
     count = count + 1
